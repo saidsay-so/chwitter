d0d179d9710509d6d1be9fad3cd773eefaf6e13d
musikid <musikid@outlook.com>
2022-03-09 14:35:45 +0100

    feat: add MessagesList

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import PropTypes from "prop-types";
 import Message from "./Message";
+import "./MessagesList.css";
 
-export default class MessagesList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const MessagesList = ({ messages }) => (
+  <section>
+    <ul className="messages">
+      {messages.map((msg) => (
+        <li key={msg.id}>
+          <Message {...msg} />
+        </li>
+      ))}
+    </ul>
+  </section>
+);
 
-  render() {
-    return (
-      <ul>
-        {this.props.messages.map((msg) => (
-          <li>
-            <Message {...msg} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+export default MessagesList;
+
+MessagesList.propTypes = {
+  messages: PropTypes.array.isRequired,
+  feedName: PropTypes.string,
+};
