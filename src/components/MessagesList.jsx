e6be05b595e4f5a87f27e655e652e9593982cf09
musikid <musikid@outlook.com>
2022-03-09 19:03:55 +0100

    feat: improve MessagesList

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import Message from "./Message";
 import "./MessagesList.css";
 
+//TODO: Add the friend handler
 const MessagesList = ({ messages }) => (
   <section>
     <ul className="messages">
@@ -18,5 +19,4 @@ export default MessagesList;
 
 MessagesList.propTypes = {
   messages: PropTypes.array.isRequired,
-  feedName: PropTypes.string,
 };
