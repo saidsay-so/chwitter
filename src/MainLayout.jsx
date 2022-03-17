da83ead647e066b0f5a90589c9ead13219136d13
musikid <musikid@outlook.com>
2022-03-17 14:30:44 +0100

    chore: move MainLayout

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
-import NavigationPanel from "../components/NavigationPanel";
+import NavigationPanel from "./components/NavigationPanel";
 import "./MainLayout.css";
-import Register from "./Register";
+import Register from "./pages/Register";
 
-const MainLayout = ({ }) => {
+const MainLayout = ({}) => {
   const [connectionState, setConnectionState] = useState(false);
 
   //TODO: Real authentication
   const authAction = () => setConnectionState(!connectionState);
 
-  const Component = connectionState ? Register : Register;
+  const Component = connectionState ? <Register /> : <Register />;
 
   return (
     <div className="main-layout">
       <NavigationPanel authAction={authAction} isConnected={connectionState} />
-      <main>
-        <Component />
-      </main>
+      <main>{Component}</main>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import MainLayout from "./pages/MainLayout";
+import MainLayout from "./MainLayout";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
