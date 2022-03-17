d5b3c839ab9c31891406bbbe9882d2fca12c1064
musikid <musikid@outlook.com>
2022-03-17 19:00:32 +0100

    feat: add api services (stub)

---
+++ src/services/messages.js
@@ -0,0 +1,30 @@
+export const createMessage = (msg) => {};
+
+export const getMessages = (user = null) => {
+  const placeholder = [
+    {
+      id: 2,
+      message: "This is a message",
+      authorPicture:
+        "http://images.guff.com/gallery/image/screen-shot-2015-09-07-at-151108",
+      author: "John",
+      authorProfileLink: "#",
+      date: "12 Jun 2021",
+      isFriend: false,
+      action: (...args) => console.log(args),
+    },
+    {
+      id: 1,
+      message: "This is a message",
+      authorPicture:
+        "http://images.guff.com/gallery/image/screen-shot-2015-09-07-at-151108",
+      author: "Not John",
+      authorProfileLink: "#",
+      date: "12 Jun 2021",
+      isFriend: true,
+      action: (...args) => console.log(args),
+    },
+  ];
+
+  return placeholder;
+};
