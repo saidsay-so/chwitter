53123013865bacb56d1107372eb596f94042397b
musikid <musikid@outlook.com>
2022-03-17 19:00:12 +0100

    fix: add handler for MessagesList

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -2,13 +2,12 @@ import PropTypes from "prop-types";
 import Message from "./Message";
 import "./MessagesList.css";
 
-//TODO: Add the friend handler
 const MessagesList = ({ messages, friendAction }) => (
   <section>
     <ul className="messages">
       {messages.map((msg) => (
         <li key={msg.id}>
-          <Message {...msg} action={friendAction} />
+          <Message {...msg} action={friendAction.bind(msg.authorId)} />
         </li>
       ))}
     </ul>
