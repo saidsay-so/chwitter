b4cd6a332b035765e6e19daeef7772b92dd962aa
musikid <musikid@outlook.com>
2022-03-23 14:27:45 +0100

    feat: add HomeFeed messages

--- src/pages/HomeFeed.jsx
+++ src/pages/HomeFeed.jsx
@@ -1,10 +1,35 @@
-import { getMessages } from "../services/messages";
+import { getMessages } from "../services/message";
 import MessagesList from "../components/MessagesList";
 import "./HomeFeed.css";
 import MessageArea from "../components/MessageArea";
+import { useAsyncEffect } from "../utils/extra-hooks";
+import { areFriends } from "../services/friend";
+import { useAuth } from "../providers/AuthProvider";
+import { useState } from "react";
 
-export default function HomeFeed({}) {
-  const messages = getMessages(null);
+export default function HomeFeed() {
+  const [messages, setMessages] = useState([]);
+  const {
+    user: { id },
+  } = useAuth();
+
+  useAsyncEffect(async (stillMounted) => {
+    const rawMessages = await getMessages();
+
+    const messages = await Promise.all(
+      rawMessages.map(async (msg) => {
+        const fromHimself = id === msg.authorId;
+        const isFriend = fromHimself
+          ? false
+          : await areFriends(id, msg.authorId);
+        return { ...msg, isFriend, fromHimself };
+      })
+    );
+
+    if (stillMounted) {
+      setMessages(messages);
+    }
+  }, []);
 
   return (
     <div className="home-feed">
