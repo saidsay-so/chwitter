0d2fba3d8a6a81f7e16c3fdbebb705d97cabad70
musikid <musikid@outlook.com>
2022-03-25 09:52:04 +0100

    feat: improve MessageArea

--- src/components/MessageArea.jsx
+++ src/components/MessageArea.jsx
@@ -1,13 +1,50 @@
 import SimpleButton from "./SimpleButton";
 import "./MessageArea.css";
+import { RiSendPlane2Fill } from "react-icons/ri";
+import { IconContext } from "react-icons";
+import { useState } from "react";
 
-export default function MessageArea({}) {
+export default function MessageArea({ id, onSubmit }) {
+  const [message, setMessage] = useState("");
+
   return (
     <div className="message-area-container">
-      <form className="message-area-form">
-        <textarea name="message" className="message-area" required></textarea>
+      <form
+        className="message-area-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          try {
+            onSubmit(message);
+            setMessage("");
+          } catch (e) {
+            console.error(e);
+          }
+        }}
+      >
+        <textarea
+          id={id}
+          name="message"
+          className="message-area"
+          placeholder="Écrire un message..."
+          onInput={(e) => {
+            setMessage(e.target.value);
+          }}
+          required
+          value={message}
+        ></textarea>
+        <SimpleButton
+          round
+          label={
+            <IconContext.Provider
+              value={{
+                style: {},
+              }}
+            >
+              <RiSendPlane2Fill />
+            </IconContext.Provider>
+          }
+        />
       </form>
-      <SimpleButton label="Envoyer" />
     </div>
   );
 }
