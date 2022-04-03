661f722b7d8f8d91596c7501385c916c6a4169d2
musikid <musikid@outlook.com>
2022-04-03 17:56:47 +0200

    feat: add like animation for Message

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./Message.css";
 import SimpleButton from "./SimpleButton";
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import User from "./User";
 import { BsHeartFill, BsHeart } from "react-icons/bs";
 import cx from "classnames";
+import { CSSTransition } from "react-transition-group";
 
 /**
  * Affiche un message
@@ -19,50 +20,58 @@ const Message = ({
   friendAction,
   fromHimself,
   likes = "69",
-  isLiked,
+  // isLiked,
   likeAction,
-}) => (
-  <article className="message-container">
-    <div className="author-info">
-      <div className="avatar-container">
-        <Avatar profileLink={profileLink} picture={picture} name={name} />
+}) => {
+  const [isLiked, setisLiked] = useState(false);
+
+  return (
+    <article className="message-container">
+      <div className="author-info">
+        <div className="avatar-container">
+          <Avatar profileLink={profileLink} picture={picture} name={name} />
+        </div>
+        <Link to={profileLink} className="author">
+          {name}
+        </Link>
+        <div className="user-details">
+          <User
+            isFriend={isFriend}
+            friendAction={friendAction ?? undefined}
+            description={description}
+            name={name}
+            picture={picture}
+            profileLink={profileLink}
+          />
+        </div>
       </div>
-      <Link to={profileLink} className="author">
-        {name}
-      </Link>
-      <div className="user-details">
-        <User
-          isFriend={isFriend}
-          friendAction={friendAction ?? undefined}
-          description={description}
-          name={name}
-          picture={picture}
-          profileLink={profileLink}
+      <p className="message">{message}</p>
+      <div className="metadata">
+        <span className={cx("likes", { isLiked })}>
+          <button
+            onClick={() => {
+              setisLiked(!isLiked);
+            }}
+            className="like-button"
+          >
+            <CSSTransition in={isLiked} timeout={100}>
+              <div className="like-icon">
+                {isLiked ? <BsHeartFill /> : <BsHeart />}
+              </div>
+            </CSSTransition>
+          </button>
+          {likes}
+        </span>
+        <span className="date">{date}</span>
+        <SimpleButton
+          label="➕ Ajouter"
+          className={cx("action", { isVisible: !(isFriend || fromHimself) })}
+          onClick={friendAction ?? undefined}
         />
       </div>
-    </div>
-    <p className="message">{message}</p>
-    <div className="metadata">
-      <span className={cx("likes", { isLiked })}>
-        <button
-          onClick={() => {
-            isLiked = true;
-          }}
-          className="like-button"
-        >
-          {isLiked ? <BsHeartFill /> : <BsHeart />}
-        </button>
-        {likes}
-      </span>
-      <span className="date">{date}</span>
-      <SimpleButton
-        label="➕ Ajouter"
-        className={cx("action", { isVisible: !(isFriend || fromHimself) })}
-        onClick={friendAction ?? undefined}
-      />
-    </div>
-  </article>
-);
+    </article>
+  );
+};
 export default Message;
 
 Message.propTypes = {
