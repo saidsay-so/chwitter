62f910c9b00ae45207530933f0b1f1cbdc7843a4
musikid <musikid@outlook.com>
2022-04-27 11:34:55 +0200

    feat(server): add support for certificate auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,17 @@ import assert from "assert";
 import app from "./app";
 import { connect } from "./database";
 
-const { SERVER_PORT, MONGO_URL } = process.env;
+const { PORT, MONGO_URL, CERT } = process.env;
 
-assert(SERVER_PORT, "Port is not defined");
+assert(PORT, "Port is not defined");
 assert(MONGO_URL, "Database URL is not defined");
 
-app.listen(parseInt(SERVER_PORT), async () => {
+app.listen(parseInt(PORT), async () => {
   try {
-    await connect(MONGO_URL);
+    await connect(MONGO_URL, { cert: CERT, key: CERT });
   } catch (e) {
     throw e;
   }
 
-  console.log(`Server listening on port ${SERVER_PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
