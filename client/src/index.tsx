e3eca60ce5a7daa3cab7dbb14696806e4d3e2881
musikid <musikid@outlook.com>
2022-04-29 19:37:53 +0200

    fix(client): remove web vitals

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./index.css";
 import MainLayout from "./layouts/MainLayout";
-import reportWebVitals from "./reportWebVitals";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import { AuthProvider } from "./providers/AuthProvider";
@@ -86,8 +85,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.info);
