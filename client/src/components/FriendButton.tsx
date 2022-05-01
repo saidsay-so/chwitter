0ad168ed48bface41e8dd2440fd184963d114597
musikid <musikid@outlook.com>
2022-05-01 16:24:28 +0200

    fix(client): improve friend button

--- client/src/components/FriendButton.tsx
+++ client/src/components/FriendButton.tsx
@@ -2,17 +2,30 @@ import SimpleButton from "./SimpleButton";
 import "./FriendButton.css";
 import cx from "classnames";
 import { ComponentPropsWithoutRef } from "react";
+import { BiUserMinus, BiUserPlus } from "react-icons/bi";
 
 /**
  * Spécialisation de [SimpleButton](#simplebutton) avec une étiquette pour l'ajout/suppression d'amis
  */
-const FriendButton = ({ isFriend, className, ...props }: FriendButtonProps) => (
-  <SimpleButton
-    {...props}
-    className={cx("friend-button", className, { "is-friend": isFriend })}
-    label={isFriend ? "➖ Supprimer des amis" : "➕ Ajouter aux amis"}
-  />
-);
+const FriendButton = ({ isFriend, className, ...props }: FriendButtonProps) => {
+  const Icon = isFriend ? BiUserMinus : BiUserPlus;
+  const label = isFriend ? "Supprimer des amis" : "Ajouter aux amis";
+
+  return (
+    <SimpleButton
+      {...props}
+      className={cx("friend-button", className, { "is-friend": isFriend })}
+      label={
+        <div>
+          <span>
+            <Icon className="friend-icon" />
+            {label}
+          </span>
+        </div>
+      }
+    />
+  );
+};
 
 export default FriendButton;
 
